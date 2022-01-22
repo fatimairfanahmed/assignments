c75ae9b6ebe0112c0ed52d1a31e989aa5c13ae8c
fatimairfanahmed <fatimairfanahmed@users.noreply.github.com>
2022-01-21 22:31:16 -0500

    assignment 0

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/*
+ * fortune.c
+ * Author: Fatima Irfan
+ * Date: 1/11/2022
+ * Print Fortune
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Life is a balance between holding on and letting go\n");
+  return 0;
+}
