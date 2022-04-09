de139b3732ad056a7df08574850d8d7b042bf15e
fatimairfanahmed <fatimairfanahmed@users.noreply.github.com>
2022-04-09 02:30:00 -0400

    assignment 9 complete

--- A09/read_ppm.c
+++ A09/read_ppm.c
@@ -1,16 +1,63 @@
+/*
+Fatima Irfan
+9th April 2022
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
 
-struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+struct ppm_pixel** read_ppm(const char* filename, int* w, int* h) {
+  int max = 0;
+  char magicNumber[32]; 
+  FILE* file = fopen(filename, "rb");
+  if (file == NULL) {
+    printf("Unable to open file %s\n", filename);
+    exit(0);
+  }
+  char buffer[256];
+  fgets(buffer, 256, file);
+  strcpy(magicNumber,buffer);
+  fgets(buffer, 256, file);
+  if (buffer[0] == '#'){
+      fgets(buffer, 256, file);
+      sscanf(buffer, "%d %d", w, h);
+  } 
+  else {
+    sscanf(buffer, "%d %d", w, h);
+  }
+  fgets(buffer, 256, file);
+  sscanf(buffer, " %d ", &max);
+  struct ppm_pixel** output;
+  output =  (struct ppm_pixel**)malloc(sizeof(struct ppm_pixel*)*(*h));
+  for (int i = 0; i < *h; i++){ 
+    output[i] = malloc(sizeof(struct ppm_pixel)*(*w)); //allocating space for each column within each row
+  } 
+  if(output == NULL) {
+    printf("Cannot allocate new memory. Exiting...\n");
+    exit(0);
+  }
 
-  // your code here
-  return NULL;
+  for (int i = 0; i < *h ; i++){
+    for (int j = 0; j < *w ; j++){
+        struct ppm_pixel temp;
+        fread(&temp, sizeof(struct ppm_pixel), 1, file);
+        output[i][j] = temp;
+    }
+  }
+  fclose(file);
+  return output;
 }
 
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
-
-  // your code here
+extern void write_ppm(const char* filename, struct ppm_pixel** pxs, int w, int h) {
+  FILE* resultFile = fopen(filename, "wb");
+  char* header = malloc(sizeof(char)*500);
+  sprintf(header, "P6\n%d %d\n255\n", w, h);
+  fwrite(header, sizeof(char), strlen(header), resultFile);
+  for (int i = 0; i < h; i++){
+      fwrite(pxs[i],sizeof(struct ppm_pixel), w, resultFile);
+  } 
+  free(header);
+  header = NULL;
+  fclose(resultFile);
 }
-
--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -1,3 +1,8 @@
+/*
+Fatima Irfan
+9th April 2022
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,7 +16,7 @@ int main(int argc, char* argv[]) {
   float xmax = 0.47;
   float ymin = -1.12;
   float ymax = 1.12;
-  int maxIterations = 1000;
+  int MAX = 1000;
 
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
@@ -28,10 +33,68 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
-  // generate pallet
   srand(time(0));
 
-  // compute image
+  struct ppm_pixel** result = (struct ppm_pixel**)malloc(sizeof(struct ppm_pixel*)*(size));
+  for (int i = 0; i < size; i++) { 
+    result[i] = malloc(sizeof(struct ppm_pixel)*(size)); 
+  } 
+
+  struct ppm_pixel* palette = (struct ppm_pixel*)(malloc(sizeof(struct ppm_pixel)*MAX));
+  for (int j = 0; j < MAX; j++){
+    palette[j].red = rand() % 255;
+    palette[j].green = rand() % 255;
+    palette[j].blue = rand() % 255;
+  }
+
+  double timer;
+  struct timeval tstart, tend;
+  gettimeofday(&tstart, NULL);
+
+  for (int r = 0; r < size; r++){
+    for (int c = 0; c < size; c++){
+      float row = r;
+      float col = c;
+      float xfrac = col/size;
+      float yfrac = row/size;
+      float x0 = xmin + xfrac*(xmax - xmin);
+      float y0 = ymin + yfrac*(ymax - ymin);
+      float x = 0;
+      float y = 0;
+      int iter = 0;
+      while (iter < MAX && x*x + y*y < 2*2){
+        float xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+      }
+
+      if (iter < MAX) {
+        result[r][c] = palette[iter];
+      }
+      else {
+        result[r][c].red = 0;
+        result[r][c].green = 0;
+        result[r][c].blue = 0;
+      }
+    }
+  }
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  printf("Computed mandelbrot set (%dx%d) in %lf seconds\n", size, size,timer);
+
+  char newFile[50];
+  int timeNow = time(0);
+  sprintf(newFile, "mandelbrot-%d-%d.ppm", size, timeNow);
+  printf("Writing file: %s\n", newFile);
+  write_ppm(newFile, result, size , size);
+
+  for (int i = 0; i < size; i++){
+    free(result[i]);
+  } 
+  free(result);
+  free(palette);
+  result = NULL;
+  palette = NULL;
 
 }
